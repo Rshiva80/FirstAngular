5a5736e5a727f2ba70c97d297f4fa3150bbe2f5d
Rshiva80 <rshiva80@gmail.com>
2025-05-12 15:33:51 -0400

    This is for 12May

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,13 +1,18 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from "./login/login.component";
+import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent],
+  imports: [LoginComponent,ProfileComponent],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss'
 })
 export class AppComponent {
   title = 'my-first-angular-app-test';
+
+  apple(){
+    alert("Alert")
+  }
 }
---
+++ src/app/profile/profile.component.ts
@@ -0,0 +1,13 @@
+import { Component } from "@angular/core";
+ 
+
+@Component ({
+    selector: 'app-profile',
+    imports: [],
+    templateUrl: './profile.component.html'
+    
+
+})
+export class ProfileComponent {
+
+}
