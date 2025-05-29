359177e24e28a96db07c2b6f546f66189c3f2418
Rshiva80 <rshiva80@gmail.com>
2025-05-28 20:28:01 -0400

    This is for 28May

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -1,18 +1,25 @@
 import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
-import { LoginComponent } from "./login/login.component";
+import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent,ProfileComponent],
+  imports: [LoginComponent, ProfileComponent],
   templateUrl: './app.component.html',
-  styleUrl: './app.component.scss'
+  styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'my-first-angular-app-test';
+  count = 0;
 
-  apple(){
-    alert("Alert")
+  handleCounter(val: string) {
+    if (val == 'plus') {
+      this.count = this.count + 1;
+    } else if (val == 'minus') {
+      if(this.count > 0){
+        this.count = this.count - 1
+      }
+    } else this.count = 0;
   }
 }
