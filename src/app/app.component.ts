80eb6bb216ab1f4e548e80aec815cb97228e8d18
Rshiva80 <rshiva80@gmail.com>
2025-07-15 18:26:53 -0400

    Added code on 15July

--- src/app/app.component.ts
+++ src/app/app.component.ts
@@ -2,24 +2,15 @@ import { Component } from '@angular/core';
 import { RouterOutlet } from '@angular/router';
 import { LoginComponent } from './login/login.component';
 import { ProfileComponent } from './profile/profile.component';
+import { RouterLink } from '@angular/router';
 
 @Component({
   selector: 'app-root',
-  imports: [LoginComponent, ProfileComponent],
+  imports: [RouterLink, RouterOutlet],
   templateUrl: './app.component.html',
   styleUrl: './app.component.scss',
 })
 export class AppComponent {
   title = 'my-first-angular-app-test';
-  count = 0;
-
-  handleCounter(val: string) {
-    if (val == 'plus') {
-      this.count = this.count + 1;
-    } else if (val == 'minus') {
-      if(this.count > 0){
-        this.count = this.count - 1
-      }
-    } else this.count = 0;
-  }
+  
 }
